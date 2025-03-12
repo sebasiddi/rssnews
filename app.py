5ae8ebd0f5f80f63328b44cbae9c87bbc92e916b
Sebastian Siddi <ssiddi@gmail.com>
2025-03-12 12:09:59 -0300

    correción de puertos para Railway

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, render_template
 import feedparser
 from datetime import datetime, timedelta
 from dateutil import parser
+import os
 
 app = Flask(__name__)
 
@@ -96,6 +97,11 @@ def news():
 def home():
     return render_template("index.html")
 
-
+""" 
 if __name__ == "__main__":
     app.run(debug=True)
+ """
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto de Railway o 5000 por defecto
+    app.run(host="0.0.0.0", port=port)
